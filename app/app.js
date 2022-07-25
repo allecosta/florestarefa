228e484f69bb2fa6373d4399c69d976579ffa078
Thiago Souza <thisouza.dev@gmail.com>
2022-07-24 21:55:26 -0300

    app updates

---
+++ app/app.js
@@ -0,0 +1,5 @@
+// const timeline_width = document.querySelectorAll()
+
+// let TimelineContainer = document.getElementById("timeline-container").clientHeight;
+
+// TimelineContainer = timeline_width;
