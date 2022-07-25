8a193a9c32e24fa580ea6731746ad485e1e6431d
Thomas Verderesi <tverderesi@gmail.com>
2022-07-24 23:58:35 -0300

    Commit to clean the mess.

--- app/app.js
+++ app/app.js
@@ -3,3 +3,21 @@
 // let TimelineContainer = document.getElementById("timeline-container").clientHeight;
 
 // TimelineContainer = timeline_width;
+
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+const themeImage = document.querySelector('#theme-image');
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        themeImage.classList.remove("bi-moon-stars-fill");
+        themeImage.classList.add("bi-sun-fill");
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        themeImage.classList.remove("bi-sun-fill");
+        themeImage.classList.add("bi-moon-stars-fill");
+    }    
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
