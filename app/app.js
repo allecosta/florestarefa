c70beb38c21980636d47dbacfd0fc185553c49bf
Thomas Verderesi <tverderesi@gmail.com>
2022-07-25 22:59:01 -0300

    Added card objects and the Supporting coding for the Add Activity button.

--- app/app.js
+++ app/app.js
@@ -16,3 +16,50 @@ function switchTheme(e) {
 }
 
 toggleSwitch.addEventListener("change", switchTheme, false);
+
+class HomeworkCard {
+  constructor(subject, deadline, content) {
+    this.id = id;
+    this.type = "Homework";
+    this.subject = subject;
+    this.deadline = deadline;
+    this.content = content;
+  }
+}
+
+class AttendanceCard {
+  constructor(date, attendance) {
+    this.id = id;
+    this.type = "Attendance";
+    this.date = date;
+    this.attendance = attendance;
+  }
+}
+
+class TestCard {
+  constructor(subject, date, content) {
+    $this.id = id;
+    this.type = "Test";
+    this.subject = subject;
+    this.date = date;
+    this.content = content;
+  }
+}
+
+SubjectListSelector = document.getElementById("SubjectList");
+SubjectList = Array("a", "b", "c", "d");
+
+SubjectListBuilder(SubjectList);
+
+function SubjectListBuilder(SubjectList) {
+  for (subject of SubjectList) {
+    console.log(subject);
+    SubjectListSelector.innerHTML =
+      SubjectListSelector.innerHTML +
+      `<option value="${subject}">${subject}</option>`;
+  }
+}
+
+oi = new TestCard("123", "Math", "01/09/2011", "Music");
+
+window.localStorage.setItem("oi", JSON.stringify(oi));
