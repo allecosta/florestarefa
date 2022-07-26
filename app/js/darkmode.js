9e9decc6cce89ee194ac4aa041765ee11cde15fd
Thomas Verderesi <tverderesi@gmail.com>
2022-07-26 18:55:57 -0300

    Modules split from main 'app.js' file.

---
+++ app/js/darkmode.js
@@ -0,0 +1,25 @@
+function darkmode() {
+  const toggleSwitch = document.querySelector(
+    '.theme-switch input[type="checkbox"]'
+  );
+  const themeImage = document.querySelector("#theme-image");
+
+  function switchTheme(e) {
+    if (e.target.checked) {
+      document.documentElement.setAttribute("data-theme", "dark");
+      themeImage.classList.remove("bi-moon-stars-fill");
+      themeImage.classList.add("bi-sun-fill");
+      document.getElementById("background").style.backgroundImage =
+        "url(./media/7.png)";
+    } else {
+      document.documentElement.setAttribute("data-theme", "light");
+      themeImage.classList.remove("bi-sun-fill");
+      themeImage.classList.add("bi-moon-stars-fill");
+      document.getElementById("background").style.backgroundImage =
+        "url(./media/3.png)";
+    }
+  }
+
+  toggleSwitch.addEventListener("change", switchTheme, false);
+}
+export { darkmode };
