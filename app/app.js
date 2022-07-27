d72694fbc94414f319b069a22565da2b18b19d03
Thomas Verderesi <tverderesi@gmail.com>
2022-07-27 17:49:02 -0300

    Inserted 'SaveActivity' module.

--- app/app.js
+++ app/app.js
@@ -1,32 +1,25 @@
 import { start_up_app } from "./js/subjectcreator.js";
 import { darkmode } from "./js/darkmode.js";
 import { progressbar } from "./js/progressbar.js";
+import { SaveActivity } from "./js/cardcreator.js";
 
 start_up_app();
 darkmode();
 progressbar();
+SaveActivity();
 
-// class HomeworkCard { constructor(subject, deadline, content) { this.id = id; this.type =
-//   "Homework"; this.subject = subject; this.deadline = deadline; this.content = content; } }
-
-// class AttendanceCard { constructor(date, attendance) { this.id = id; this.type = "Attendance";
-//   this.date = date; this.attendance = attendance; } }
-
-// class TestCard { constructor(subject, date, content) { this.id = id; this.type = "Test";
-//   this.subject = subject; this.date = date; this.content = content; } }
+// class HomeworkCard { constructor(id, subject, deadline, content, xp, attendance) { this.id = id;
+//   // contador automatico this.type = "Homework"; //varchar this.subject = subject; //varchar like
+//     type this.deadline = deadline; //date this.content = content; //varchar this.xp = xp;
+//     //number this.attendance = this.attendance; //boolean } }
 
 // oi = new TestCard("123", "Math", "01/09/2011", "Music");
 
 // window.localStorage.setItem("oi", JSON.stringify(oi));
 
-function handleFormSubmit(event) {
-  event.preventDefault();
+// function handleFormSubmit(event) { event.preventDefault();
 
-  const data = new FormData(event.target);
+//   const data = new FormData(event.target);
 
-  const formJSON = Object.fromEntries(data.entries());
-  console.log("");
-}
-
-const form = document.querySelector(".contact-form");
-form.addEventListener("submit", handleFormSubmit);
+//   const formJSON = Object.fromEntries(data.entries()); console.log("");
+// }
