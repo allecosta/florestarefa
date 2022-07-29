12bb690e58c051a7802f0c93f868dd17e063eb77
Thomas Verderesi <tverderesi@gmail.com>
2022-07-28 23:54:22 -0300

    Created Function CardLoader in order to animate the cards correctly.

--- app/js/cardloader.js
+++ app/js/cardloader.js
@@ -1,14 +1,35 @@
 import { ObjectFromCard } from "./cardobject.js";
 
+//Adding procedural animation
+function CardLoader(callback) {
+  let CardsStrings = localStorage.getItem("cards");
+  let CardsArray = CardsStrings.split(",");
+  for (let i = 0; i < CardsArray.length; i++) {
+    document.styleSheets[3].insertRule(
+      `.slide-in-left div:nth-of-type(${i + 1}) {
+      animation: slide-in-left 0.7s ease-in-out both;
+      animation-delay: ${0 + 0.3 * i}s;
+      
+  }`,
+      4 + i
+    );
+  }
+
+  callback();
+}
+
+//Loading Cards from Database
 function LoadCards() {
+  //Parsing Cards Array
   let CardsStrings = localStorage.getItem("cards");
   let CardsArray = CardsStrings.split(",");
 
   for (let CardId of CardsArray) {
+    //Parsing each card
     let CardAtt = JSON.parse(localStorage.getItem("card_data" + CardId));
-
     let CardObject = ObjectFromCard(CardAtt);
 
+    //FINALLY building a card
     $("#cards-container").append(`
       <div id=${CardObject.id} class="card container-fluid rounded mt-3">
       <div class="card-body row">
@@ -23,19 +44,18 @@ function LoadCards() {
                   font-weight-bold">${CardObject.deadline}</button>
                   <button type="button" class="btn btn-exp btn-sm
                             font-weight-bold">XP: ${CardObject.xp} </button>
+                  <div class="col-1 task-checkbox align-self-center justify-self-center">
+                  <input name="done" class="xp_done done" type="checkbox" value="${CardObject.xp}">
+                  </div>
+                  
               </div>
 
               <p class="card-text mt-3 text-justify">${CardObject.content}</p>
           </div>
 
-          <div class="col-1 task-checkbox justify-self-center">
-          <input name="done" class="done" type="checkbox" value="10">
-          </div>
 
-      </div>
-  </div>`);
-    console.log("cabou iteraçao");
+      </div>`);
   }
 }
 
-export { LoadCards };
+export { CardLoader, LoadCards };
