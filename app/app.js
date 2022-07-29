f8d95db54c688083d98edf3d048ecd9587f2a422
Thomas Verderesi <tverderesi@gmail.com>
2022-07-28 23:52:13 -0300

    Almost there: Renamed some modules. Finished Profile Service.

--- app/app.js
+++ app/app.js
@@ -1,28 +1,19 @@
 import { start_up_app } from "./js/subjectcreator.js";
-import { darkmode } from "./js/darkmode.js";
-import { progressbar } from "./js/progressbar.js";
 import { SaveActivity } from "./js/cardcreator.js";
-import { LoadCards } from "./js/cardloader.js";
+import { CardLoader, LoadCards } from "./js/cardloader.js";
+import { darkmode } from "./js/darkmode.js";
+import { GameService } from "./js/gameservice.js";
+import { ProfileService } from "./js/profileservice.js";
 
-start_up_app();
+CardLoader(LoadCards);
+ProfileService();
 darkmode();
-progressbar();
+GameService();
+start_up_app();
 SaveActivity();
-LoadCards();
 
 $(document).ready(function () {
   $("#AddActivitybtn").click(function () {
     $("#AddActivityContainer").toggle();
   });
 });
-
-// oi = new TestCard("123", "Math", "01/09/2011", "Music");
-
-// window.localStorage.setItem("oi", JSON.stringify(oi));
-
-// function handleFormSubmit(event) { event.preventDefault();
-
-//   const data = new FormData(event.target);
-
-//   const formJSON = Object.fromEntries(data.entries()); console.log("");
-// }
